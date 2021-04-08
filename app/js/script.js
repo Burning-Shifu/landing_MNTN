32dbbe1c962b8fa2b1172b98f7c9c42317a43332
Shifu <kris.deniso@mail.ru>
2021-04-08 22:50:43 +0300

    start project

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+//Подключение js других страниц или элементов/////////////////////////////////////////
+// @@include('forms.js', {})
+
+//Основной блок//////////////////////////////////////////////////////
+
+// $(window).on('scroll', function () {
+// 	if ($(this).scrollTop() > 20) {
+// 		(!$('header').hasClass('class'))
+// 		$('header').addClass('class');
+// 	}
+// 	else {
+// 		$('header').removeClass('class');
+// 	}
+// });
