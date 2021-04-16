f3644caaed1276054184654234e1e3511d4e3eac
Shifu <kris.deniso@mail.ru>
2021-04-17 00:39:43 +0300

    added adaptive & dynamic id to artcls & navbar

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,31 @@
-//Подключение js других страниц или элементов/////////////////////////////////////////
-// @@include('forms.js', {})
+document.addEventListener('DOMContentLoaded', () => {
+  const articles = document.querySelectorAll('.article'),
+        navbar = document.querySelector('.navbar'),
+        navbarList = document.querySelector('.navbar__list');
 
-//Основной блок//////////////////////////////////////////////////////
+  // фон у navbar при прокрутке для контраста
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+      navbar.classList.add('navbar-bg');
+    } else {
+      navbar.classList.remove('navbar-bg');
+    }
+  });
 
+  // генерация li > navbar__num и атрибута id для статей
+  for (let i = 0; i < articles.length; i++) {
+    let id = i + 1;
+    const navbarItem = document.createElement('li');
+
+    if (i < 10) {
+      id = `0${id}`
+    }
+    navbarItem.innerHTML = `
+      <a class="btn navbar__num" href="#${id}">${id}</a>
+    `;
+
+    navbarList.append(navbarItem);
+
+    articles[i].setAttribute('id', id)
+  }
+});
